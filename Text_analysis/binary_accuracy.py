e7cefc0d4a781fc21df29fa9af92f27b805056fe
Dmitri Kochubei <dmitri.kochubei@gmail.com>
2020-07-13 16:12:53 +0300

    Delete binary_accuracy.py

--- Text_analysis/binary_accuracy.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-def binary_accuracy(preds, y):
-    #round predictions to the closest integer
-    rounded_preds = torch.round(preds)    
-    correct = (rounded_preds == y).float() 
-    acc = correct.sum() / len(correct)
-    return acc
